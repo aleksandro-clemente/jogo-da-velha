26cd658bfd35c37737d0d5f19d70b471d1e79b7f
Aleksandro Clemente <aleksandro1404@gmail.com>
2023-05-16 00:16:08 -0300

    Adicionado 9 botões

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -1,13 +1,25 @@
+import java.awt.Button;
+
 import javax.swing.*;
 
 public class JogoDaVelha extends JFrame {
 	
+	JButton bt[] = new JButton[9];
 	public JogoDaVelha() {
 		setVisible(true);
 		setTitle("Jogo da Velha");
 		setDefaultCloseOperation(3);
 		setLayout(null);
 		setBounds(250,100,700,500);
+		int cont = 0;
+		for(int i=0;i<3;i++) {
+			for(int j = 0; j<3; j++) {
+				bt[cont] = new JButton();
+				add(bt[cont]);
+				bt[cont].setBounds((100 *i)+50, (100*j) +50, 95, 95);
+				cont++;
+			}
+		}
 	}
 	public static void main(String[] args) {
 		new JogoDaVelha();
